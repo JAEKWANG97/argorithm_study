7c49442b38f6ba038c7044be17c74f74aa1bf9dc
JAEKWANG97 <65598179+JAEKWANG97@users.noreply.github.com>
2022-03-01 18:38:28 +0900

    2022.03.01_1
    문교랑 카페에서

--- PythonAlgorithm/leetcode/leetcode_937.py
+++ PythonAlgorithm/leetcode/leetcode_937.py
@@ -2,8 +2,21 @@
 # 2. 문자로 구성된 로그가 숫자 로그보다 앞에 온다.
 # 3. 식별자는 순서에 영향을 끼치지 않지만, 문자가 동일할 경우 식별자 순으로 한다.
 # 4. 숫자 로그는 입력 순서대로 한다.
-
-
-log = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]
+from unicodedata import digit
+logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]
 # 출력 ["let1 art can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1", "dig2 3 6"]
 
+digits = []
+letters = []
+
+for log in logs:
+    if log.split()[1].isdigit():
+        digits.append(log)
+    else:
+        letters.append(log)
+
+print(digits)
+print(letters)
+
+letters.sort(key = lambda x : (x.split()[1:], x.split()[0]))
+print(letters  + digits)
