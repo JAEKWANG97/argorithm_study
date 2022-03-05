cee22c315dbe3331690688b96af4298f0a5ed86c
JAEKWANG97 <65598179+JAEKWANG97@users.noreply.github.com>
2022-03-06 00:18:46 +0900

    ㅇㅇ

--- PythonAlgorithm/leetcode/leetcode_819.py
+++ PythonAlgorithm/leetcode/leetcode_819.py
@@ -1,41 +1,15 @@
 # 금지된 단어를 제외한 가장 흔하게 등장하는 단어를 출력하라. 대소문자 구분을 하지 않으며, 구두점 또한 무시한다.
 
-from ast import Pass
-import collections
-import re
 
-from matplotlib.collections import Collection
+import re
 
 
 paragraph = "Bob hit a ball, the hit BALL flew far after ,,it was hit"
-paragraph = paragraph.lower()
 banned = ["hit"]
-paragraph = paragraph.split()
-s = re.sub('[^a-z0-9]', ' ', str(paragraph))
-s = s.split()
 
-for value in banned:
-    s.remove(value)
+words = [word for word in re.sub(r'^r|w)', ' ' , paragraph).lower().split() if word not in banned]
 
-count = {}
-
-for i in s:
-    try: count[i] += 1
-    except: count[i] = 1
-max_value = max(count.values())
-
-print(max_value)
-
-for i in range(len(count)):
-    Pass()
-    
-
-
-
-
-
-
-
+print(words)
 
 
 
