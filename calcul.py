cab99562818c69625317328de864bfa7b8e367a0
JAEKWANG97 <65598179+JAEKWANG97@users.noreply.github.com>
2022-03-08 15:48:24 +0900

    칼로리 계산 수정

--- calcul.py
+++ calcul.py
@@ -2,20 +2,21 @@ from msvcrt import putch
 from this import d
 from turtle import onscreenclick
 
+# 자신의 신체 정보를 입력해주세요.
+# height = int(input('키를 입력하시오 : '))
+# weight = int(input('몸무게를 입력하시오 : '))
+# age = int(input('나이를 입력하시오: '))
 
-height = int(input('키를 입력하시오'))
-weight = int(input('몸무게를 입력하시오'))
+height = int(172)
+weight = int(82)
+age = int(26)
 
-
-value = 66+(13.7*weight)+(5*height) - 6.8*26
-
+# 기초 대사량(헤리슨 베네딕트 방정식)
+value = 66+(13.7*weight)+(5*height) - 6.8*age
+# 유지
 Need_value = value * 1.55
 
-print(Need_value)
-
 purpose_value = Need_value*0.8
-
-print(purpose_value)
 
 carbon_cal = round(purpose_value*0.5/4)
 
@@ -26,6 +27,8 @@ fat_cal = round(purpose_value*0.2/9)
 diet_cal = {'탄수화물 그람' : carbon_cal, '단백질 그람' : protein_cal, '지방 그람' : fat_cal}
 
 onece_meal = {'탄수화물 그람' : carbon_cal/4, '단백질 그람' : protein_cal/4, '지방 그람' : fat_cal/4}
-print(diet_cal)
-print(onece_meal)
+print('하루 총 열량 : ', diet_cal)
+print('식사 한끼 열량 : ' , onece_meal)
+
+
 
