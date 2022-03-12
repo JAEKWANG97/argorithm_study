4d72044f7b8326d86e421eaf6466d60559b71928
JAEKWANG97 <65598179+JAEKWANG97@users.noreply.github.com>
2022-03-12 22:34:58 +0900

    Update leetcode_819.py

--- PythonAlgorithm/leetcode/leetcode_819.py
+++ PythonAlgorithm/leetcode/leetcode_819.py
@@ -1,16 +1,22 @@
-# 금지된 단어를 제외한 가장 흔하게 등장하는 단어를 출력하라. 대소문자 구분을 하지 않으며, 구두점 또한 무시한다.
+# 금지된 단어를 제외한 가장 흔하게 등장하는 단어를 출력하라. 
+# 대소문자 구분을 하지 않으며, 구두점 또한 무시한다.
 
 
+import collections
 import re
 
 
 paragraph = "Bob hit a ball, the hit BALL flew far after ,,it was hit"
 banned = ["hit"]
 
-words = [word for word in re.sub(r'^r|w)', ' ' , paragraph).lower().split() if word not in banned]
+words = [word for word in re.sub(r'[^|w]', ' ' , paragraph)
+    .lower().split() 
+        if word not in banned]
 
-print(words)
+counts = collections.Counter(words)
 
 
+
+print(counts.most_common(1)[0][0])
 
 # 출력 ㅣ "ball"
