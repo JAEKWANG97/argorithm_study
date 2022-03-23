710f062c3565a4ed7d87e225eb49b9202cae4ddf
JAEKWANG97 <65598179+JAEKWANG97@users.noreply.github.com>
2022-03-23 19:13:43 +0900

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -1,6 +1,4 @@
-from msvcrt import putch
-from this import d
-from turtle import onscreenclick
+
 
 # 자신의 신체 정보를 입력해주세요.
 # height = int(input('키를 입력하시오 : '))
@@ -14,8 +12,9 @@ age = int(26)
 # 기초 대사량(헤리슨 베네딕트 방정식)
 value = 66+(13.7*weight)+(5*height) - 6.8*age
 # 유지
-Need_value = value * 1.55
-
+Need_value = round(value * 1.55)
+print("유지 칼로리 ", Need_value)
+#증량을 위한 칼로리 
 purpose_value = Need_value*0.8
 
 carbon_cal = round(purpose_value*0.5/4)
@@ -28,7 +27,7 @@ diet_cal = {'탄수화물 그람' : carbon_cal, '단백질 그람' : protein_cal
 
 onece_meal = {'탄수화물 그람' : carbon_cal/4, '단백질 그람' : protein_cal/4, '지방 그람' : fat_cal/4}
 print('하루 총 열량 : ', diet_cal)
-print('식사 한끼 열량 : ' , onece_meal)
+print('식사 한끼 열량(4끼) : ' , onece_meal)
 
 
 
