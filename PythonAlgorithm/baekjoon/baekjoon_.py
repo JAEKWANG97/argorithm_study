212ae8561933e8edd9381121b6a152f94e71d8e5
JAEKWANG97 <65598179+JAEKWANG97@users.noreply.github.com>
2022-04-15 00:01:29 +0900

    ddd

---
+++ PythonAlgorithm/baekjoon/baekjoon_.py
@@ -0,0 +1,12 @@
+i = int(100)
+
+print(str(i))
+
+
+natural_num =set(range(1,10001))
+
+generated_num = set()
+j = str('abcdef')
+
+for i in j:
+    print(i)
---
+++ PythonAlgorithm/leetcode/leetcode_49.py
@@ -0,0 +1,4 @@
+str_list = ["eat","tea","tan", "ate", "nat", "bat"]
+
+for i in range(len(str_list)):
+    
