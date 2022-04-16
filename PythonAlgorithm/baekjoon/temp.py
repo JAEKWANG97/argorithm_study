e5baa346ac84ca8afed99bf46b25b0d709c272d8
Picka <65598179+JAEKWANG97@users.noreply.github.com>
2022-04-16 23:28:24 +0900

    삽질 완료

--- PythonAlgorithm/baekjoon/temp.py
+++ PythonAlgorithm/baekjoon/temp.py
@@ -1,14 +1,31 @@
 from itertools import combinations_with_replacement, product
-from more_itertools import flatten
-dataset = [0,1,2,3,]
-num =int(11)
-# 2개를 뽑아 일렬로 나열하는 경우의 수(단, 중복 허용)
-res = list(combinations_with_replacement(dataset, num))
+import math
+
+dataset = []
+num =int(41)
+
+for i in range(num):
+    if int(math.pow(i,2)) < num :
+        dataset.append(i)
+
+# 개를 뽑아 일렬로 나열하는 경우의 수(단, 중복 허용)
+res = list(combinations_with_replacement(dataset,num))
 temp = int(0)
+key = []
+theShortestKey = []
+for i in res:
+    temp = 0 
+    for j in i:
+        if j != 0:
+            temp = temp + int(math.pow(j,2))
+    if temp == num:
+        list(i).count(0)
+        key.append(list(i))
 
-# for i in res:
-#     print(type(i))
+temp2 = 0
+for i in key:
+    if i.count(0) > temp2:
+        theShortestKey = i
+        temp2 = i.count(0)
 
-# print(f"모든 경우: {res}")
-# print(f"모든 경우의 수: {len(res)}") # 9
-
+print(num - theShortestKey.count(0))
