ff2cfdde97694a386e2d29d289eae320c12731cf
JAEKWANG97 <65598179+JAEKWANG97@users.noreply.github.com>
2022-04-22 15:57:43 +0900

    hello starbucks

--- PythonAlgorithm/leetcode/leetcode_1_2.py
+++ PythonAlgorithm/leetcode/leetcode_1_2.py
@@ -1,16 +1,7 @@
-
-
-
-from itertools import combinations
-
-
 class Solution:
     def twoSum(self,nums : list,target:int)->int:
-        # print(list(combinations(nums,2)))
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+            
         
         
         
