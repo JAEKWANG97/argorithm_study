1da3b69c2bcd685246bfb79fc55a97235334e1e4
Picka <65598179+JAEKWANG97@users.noreply.github.com>
2022-05-09 22:18:32 +0900

    백준 1978 완료

--- PythonAlgorithm/baekjoon/baekjoon_1978.py
+++ PythonAlgorithm/baekjoon/baekjoon_1978.py
@@ -1,30 +1,15 @@
 # 주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
 
 # 첫 줄에 수의 개수 N이 주어진다. N은 100이하이다. 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
-import collections
-
 n = int(input())
-nList = input().split()
-nList = list(map(int,nList))
-if 1 in nList:
-    nList.remove(1)
-
-
-# int(input())
-
-
-
-primeNum = []
-
-primeNum = list(collections.defaultdict(int))
-
-for i in nList:
-    for j in range(1,int(nList[-1])):
-        if i*j in nList: 
-            nList[nList.index(i*j)] = 0
-            if i not in primeNum:
-                primeNum.append(i)
-
-
-
+nList = map(int , input().split())
+primeNum = 0
+for num in nList:
+    primeNumValue = True
+    if num > 1:
+        for i in range(2,num):
+                if num % i == 0:
+                    primeNumValue = False
+        if primeNumValue == True:
+            primeNum += 1
 print(primeNum)
