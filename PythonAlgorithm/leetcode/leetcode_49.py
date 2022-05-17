54ea095342ef0babd99e9b26227e572ddf3f552a
JAEKWANG97 <65598179+JAEKWANG97@users.noreply.github.com>
2022-05-17 15:58:13 +0900

    Update leetcode_49.py

--- PythonAlgorithm/leetcode/leetcode_49.py
+++ PythonAlgorithm/leetcode/leetcode_49.py
@@ -1,11 +1,14 @@
 import collections
-
+# 문자열을 받아 애너그램단위로 그룹핑 하라
 
 
 strs = ["eat","tea","tan", "ate", "nat", "bat"]
+
+# 그룹핑 후 담을 변수
 anagrams = collections.defaultdict(list)
+
 for word in strs:
-    # print(''.join(sorted(word)))
+
     anagrams[''.join(sorted(word))].append(word)
 
-print(list(anagrams))
+print(anagrams.values())
