2ffcba2f31227b5a3df268813ce86736dc8ff2dc
JAEKWANG97 <65598179+JAEKWANG97@users.noreply.github.com>
2022-06-05 15:24:21 +0900

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -5,9 +5,9 @@
 # weight = int(input('몸무게를 입력하시오 : '))
 # age = int(input('나이를 입력하시오: '))
 
-height = int(162)
-weight = int(47)
-age = int(27)
+height = int(170)
+weight = int(80)
+age = int(26)
 
 # 기초 대사량(헤리슨 베네딕트 방정식)
 value = 66+(13.7*weight)+(5*height) - 6.8*age
@@ -16,26 +16,28 @@ print("기초대사량", round(value))
 Need_value = round(value * 1.55)
 print("유지 칼로리 ", Need_value)
 #증량을 위한 칼로리 
-purpose_value = Need_value*0.8
+purpose_value = Need_value*1.2
 
-carbon_cal = round(purpose_value*0.5/4)
+carbon_cal = round(purpose_value*0.5)
 
-protein_cal = round(purpose_value*0.3/4)
+protein_cal = round (purpose_value*0.3)
 
-fat_cal = round(purpose_value*0.2/9)
+fat_cal = round(purpose_value*0.2)
 
-diet_cal = {'탄수화물 그람' : carbon_cal, '단백질 그람' : protein_cal, '지방 그람' : fat_cal}
+carbon_gram = carbon_cal/4
 
-onece_meal = {'탄수화물 그람' : carbon_cal/4, '단백질 그람' : protein_cal/4, '지방 그람' : fat_cal/4}
-print('하루 총 열량 : ', diet_cal)
-print('식사 한끼 열량(4끼) : ' , onece_meal)
+protein_gram = protein_cal/4
+
+fat_gram = round(fat_cal/9)
+
+diet_gram = {'탄수화물 그람' : carbon_gram, '단백질 그람' : protein_gram, '지방 그람' : fat_gram}
+
+onece_meal = {'탄수화물 그람' : carbon_cal/5, '단백질 그람' : protein_cal/5, '지방 그람' : fat_cal/5}
 
 
+print('하루 총 열량 : ', diet_gram)
+print('식사 한끼 열량(5끼) : ' , onece_meal)
 
-#심박수에 따른 운동 법
-bpm = int(0)
-age = 26
-max_bpm = int(220) - age
 
 
 
