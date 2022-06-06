7c0c3e9232696fa05a520d68754c94ea8d8a7fff
Picka <65598179+JAEKWANG97@users.noreply.github.com>
2022-06-06 13:14:07 +0900

    Update leetcode_5.py

--- PythonAlgorithm/leetcode/leetcode_5.py
+++ PythonAlgorithm/leetcode/leetcode_5.py
@@ -22,10 +22,22 @@ def longestPalindrome(s : str) -> str:
     if len(s) <2 or s == s[::-1]:
         return s
 
-        result = ''
+    result = ''
         #슬라이딩 윈도우 우측으로 이동
+<<<<<<< HEAD
+
+    for i in range(len(s) - 1):
+        result = max(result, expand(i , i+1), expand(i,i+2),key= len)
+        return result
+
+
+=======
         for i in range(len(s) - 1):
             result = max(result, expand(i , i+1), expand(i,i+2),key= len)
+<<<<<<< Updated upstream
+=======
+>>>>>>> 531aea833ef47a30d274a0b6f2229067fcaf7162
+>>>>>>> Stashed changes
 print(longestPalindrome(s))
 
 
