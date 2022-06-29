d38558f36c32bedb601b588da62b206ca7005a16
Picka <65598179+JAEKWANG97@users.noreply.github.com>
2022-06-30 02:38:55 +0900

    Update leetcode_15_1.py

--- PythonAlgorithm/leetcode/leetcode_15_1.py
+++ PythonAlgorithm/leetcode/leetcode_15_1.py
@@ -4,7 +4,7 @@
 
 
 
-nums = [-1,0,1,2,-1,-4]
+nums = [0,0,0]
 
 # 출력 = [ [-1,0,1] , [-1,-1,2] ]
 
@@ -12,14 +12,30 @@ value = []
 
 nums.sort()
 
-for i in range(len(nums)):
-    for j in range(i+1,len(nums)):
-        for k in range(j+1,len(nums)):
-            if nums[i] + nums[j] + nums[k] == 0:
-                print(nums[i],nums[j],nums[k])
-                temp = [nums[i],nums[j],nums[k]]
-                if temp not in value:
-                    value.append(temp)
+sum_zero = []
+
+for i in range(len(nums)-2):
+    
+    for j in range(i+1,len(nums)-1):
+        if i == 0 and j == 0 and 0 in nums:
+            temp2=[0,0,0]
+            sum_zero.append(temp2)
+        
+        first_value = nums[i]
+        second_value = nums[j]
+        if -(first_value + second_value) in nums[j+1:] :
+            
+            temp2 = [first_value,second_value,-(first_value + second_value)]
+            if temp2 not in sum_zero:
+                sum_zero.append(temp2)
 
 
+print(sum_zero)
 
+
+# for k in range(j+1,len(nums)):
+#             if nums[i] + nums[j] + nums[k] == 0:
+#                 print(nums[i],nums[j],nums[k])
+#                 temp = [nums[i],nums[j],nums[k]]
+#                 if temp not in value:
+#                     value.append(temp)
